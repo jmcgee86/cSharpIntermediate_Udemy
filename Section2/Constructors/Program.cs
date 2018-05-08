9ffcbbd23090169d0fa2c1b511723a1683f6103b
Jim McGee <jmcgee86@gmail.com>
2018-05-08 06:51:13 -0400

    section 2.9 complete

---
+++ Section2/Constructors/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Constructors
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer(1,"John");
+
+            var order = new Order();
+            customer.Orders.Add(order);
+
+            System.Console.WriteLine(customer.Id);
+            System.Console.WriteLine(customer.Name);
+        }
+    }
+}
---
+++ Section2/Methods/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Methods
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var number = int.Parse("abc");
+            int number;
+            var result = int.TryParse("abc", out number);
+            if (result)
+                System.Console.WriteLine(number);
+            else
+            {
+                System.Console.WriteLine("conversion failed");
+            }
+        }
+
+        static void UseParams()
+        {
+            var calculator = new Calculator();
+            System.Console.WriteLine(calculator.Add(1,2));
+        }
+
+        static void UsePoints()
+        {
+            try
+            {
+                var point = new Point(10,20);
+                //point.Move(new Point(40,60));
+                point.Move(null);
+                System.Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+
+                point.Move(100,200);
+                System.Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y); 
+            }
+            catch (System.Exception)
+            {
+                System.Console.WriteLine("An unexpecter error occured");
+            }
+        }
+    }
+}
