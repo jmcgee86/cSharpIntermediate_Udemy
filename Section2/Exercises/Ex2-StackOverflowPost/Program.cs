d50e0f48dc46cec420e527b6f6ef24fd2c0ddee7
Jim McGee <jmcgee86@gmail.com>
2018-05-10 15:48:17 -0400

    Section 2 complete

---
+++ Section2/Exercises/Ex2-StackOverflowPost/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+
+namespace Ex2_StackOverflowPost
+{
+    public class Post
+    {
+        private string _title {get; set;}
+        private string _description {get; set;}
+        public readonly DateTime CreatedOn = DateTime.Now;
+        private int _upVote = 0;
+        private int _downVote = 0;
+
+        public void SetTitle(string title)
+        {
+            _title = title;
+        }
+
+        public void SetDescription(string description)
+        {
+            _description = description;
+        }
+
+        public void UpVote()
+        {
+            _upVote++;
+        }
+
+        public void DownVote()
+        {
+            _downVote++;
+        }
+
+        public int VoteTotal()
+        {
+            return _upVote - _downVote;
+        }
+        
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var post1 = new Post();
+            System.Console.WriteLine(post1.CreatedOn);
+
+            post1.UpVote();
+            post1.UpVote();
+            post1.DownVote();
+            post1.UpVote();
+            System.Console.WriteLine(post1.VoteTotal());
+
+
+        }
+    }
+}
