9cd4d53f13e51fd08ed08f224f05e40c5f823a79
Jim McGee <jmcgee86@gmail.com>
2018-05-10 15:11:57 -0400

    Section 2 - Exercise 1: Stopwatch complete

--- Section2/Exercises/Ex1-Stopwatch/Program.cs
+++ Section2/Exercises/Ex1-Stopwatch/Program.cs
@@ -1,12 +1,58 @@
 ﻿using System;
+using System.Threading;
 
 namespace Ex1_Stopwatch
 {
+    public class StopWatch
+    {
+    private DateTime StartTime { get;  set; }
+    private DateTime StopTime { get;  set; }
+
+    private bool started = false;
+
+         public void Start(DateTime start)
+        {
+            if (!started)
+            {
+                StartTime = start;
+                started = true;
+            }
+            else
+            {
+                throw new InvalidOperationException("Already started the stopwatch");
+            }
+        }
+        public void Stop (DateTime stop)
+        { 
+            if(started)
+            {
+                StopTime = stop;
+                started = false;
+            }        
+        }
+
+        public TimeSpan getDuration()
+        {
+            
+                var duration = StopTime - StartTime;
+                return duration;
+            
+            
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var stopWatch = new StopWatch();
+            stopWatch.Start(DateTime.Now);
+            
+            Thread.Sleep(5000);
+            
+            stopWatch.Stop(DateTime.Now);
+
+            System.Console.WriteLine(stopWatch.getDuration());
+
         }
     }
 }
